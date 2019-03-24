0249861619cf9f22fbdb1d3d37cb79f279b2cbeb
Andrey <killerus34@gmail.com>
2019-03-24 18:17:37 +0300

    1.5

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int A , B;
+    int A , B, C;
     printf("\nEnter A\n");
     scanf("%d",&A);
     printf("Enter B\n");
     scanf("%d",&B);
+    C=A+B;
+    printf("%d + %d = %d",A,B,C);
     return 0;
 }
